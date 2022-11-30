81f021c881277d6f5671d463270e57d749ad89af
gtrogi <gtrogi@gmail.com>
2022-11-29 20:41:01 -0600

    change Host and stuff

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ const server = http.createServer(async (req, res) => {
 //note: process.env.PORT picks up the environment variable so if you
 //      install on a different server and they set the PORT it will
 //      listen to that port instead
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3003;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
