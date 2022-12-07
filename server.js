41cc8660a2062864a4905b839af7372bb87fdb4a
gtrogi <gtrogi@gmail.com>
2022-12-07 15:31:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const server = http.createServer(async (req, res) => {
 
     //end the response with the correct return message (always of type json)
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Content-Type', 'application/json');
     res.writeHead(code);
     res.end(message);
